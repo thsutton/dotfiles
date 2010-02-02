514da2d9925fe8323446d9d81d518c0ba2cdec91
Thomas Sutton <me@thomas-sutton.id.au>
2010-02-02 12:01:49 +0800

    Fix pathmunge on OS X

--- .bash/lib/paths.sh
+++ .bash/lib/paths.sh
@@ -3,7 +3,7 @@
 # Functions for manipulating $PATH style strings.
 #
 pathmunge () {
-    if ! echo $PATH | /bin/egrep -q "(^|:)$1($|:)" ; then
+    if ! echo $PATH | egrep -q "(^|:)$1($|:)" ; then
         if [ -d "$1" ] ; then 
             if [ "$2" = "after" ] ; then
                 PATH="$PATH:$1"
