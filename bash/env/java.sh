088b864c8580ac8659997dd1b1c9ec0b4321f412
Thomas Sutton <me@thomas-sutton.id.au>
2019-03-12 10:20:55 +1100

    Set JAVA_HOME and add Homebrew gettext to PATH

---
+++ bash/env/java.sh
@@ -0,0 +1,3 @@
+if [ -e "/usr/libexec/java_home" ]; then
+export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
+fi
--- bash/env/path.sh
+++ bash/env/path.sh
@@ -4,6 +4,7 @@ done
 
 pathmunge "/opt/local/bin" "move"
 pathmunge "/usr/local/bin" "move"
+pathmunge "/usr/local/opt/gettext/bin"
 pathmunge "$HOME/Library/Haskell/bin" "move"
 pathmunge "$HOME/Library/bin"
 pathmunge "$HOME/Library/bin"
